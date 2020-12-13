c332e03bdabd615c70f4828490190a50bfc54bc3
Ben Mora <blackopsben@gmail.com>
2020-12-13 14:39:44 -0600

    Flying Enemy

---
+++ Assets/FlyLeft.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyLeft : MonoBehaviour
+{
+    private float speed = 2f;
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+    }
+}
