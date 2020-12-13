c0787d93420863fb677a53eb387cff24613532be
Ben Mora <blackopsben@gmail.com>
2020-12-13 12:02:10 -0600

    Ceiling

---
+++ Assets/DamagePlayerOnTouch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayerOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerManager>())
+        {
+            if (PlayerManager.Instance.invincible.GetIsInvincible() == false)
+            {
+                CardManager.Instance.GainBadCard();
+            }
+        }
+    }
+}
