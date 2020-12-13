d8062f9acb0d215bbffa9b4b07882cc2e1870ce9
Ben Mora <blackopsben@gmail.com>
2020-12-13 15:59:52 -0600

    Dash animation

--- Assets/Dash.cs
+++ Assets/Dash.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    private float dashSpeedMultiplier = 4f;
+
+    private float dashDuration = 0.25f;
+
+    private float timer = 0f;
+
+    public bool isDashing = false;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > dashDuration)
+        {
+            GameManager.Instance.ResetScrollSpeed();
+            isDashing = false;
+            PlayerManager.Instance.animator.SetTrigger("DashEnd");
+        }
+    }
+
     public void ExecuteDash()
     {
-        Debug.Log("Player executed dash!");
+        PlayerManager.Instance.animator.SetTrigger("Dash");
+        isDashing = true;
+        timer = 0f;
+        GameManager.Instance.SetScrollSpeed(GameManager.Instance.GetScrollSpeed() * dashSpeedMultiplier);
     }
 }
